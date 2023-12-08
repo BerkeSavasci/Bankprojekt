15d3bff7a7217af79ddbd60242ca2a83a92a624b
Berke <brk.svasci@gmail.com>
2023-12-09 00:23:03 +0100

    "Added detailed comments and getter/setter methods to Aktie class
    The Aktie class was lacking detailed comments for almost all fields/variables and methods, limiting future understanding and manageability of the code. To improve code readability and maintainability, detailed comments were added to explain the purpose and usage of each class field and method. Also, additional getter and setter methods have been added, allowing for wider interaction with the class's data from outside entities."

--- src/main/java/bankprojekt/verarbeitung/Aktie.java
+++ src/main/java/bankprojekt/verarbeitung/Aktie.java
@@ -11,13 +11,39 @@ import java.util.concurrent.TimeUnit;
  * project: Uebung_3_new
  */
 public class Aktie {
+    /**
+     * Represents the name of an object.
+     * <p>
+     * The name is a private String variable that holds the name of an object. It is used to identify and reference the object.
+     * </p>
+     * <p>
+     * <strong>Note:</strong> The name should be unique within the context of the object.
+     * </p>
+     * <p>
+     * This variable is private, meaning it can only be accessed from within the declared class.
+     * </p>
+     */
     private String name;
+    /**
+     * The wertpapierkennummer represents the unique identifier for a security.
+     */
     private int wertpapierkennummer;
+    /**
+     * This variable represents the exchange rate of a currency.
+     */
     private double kurs;
     private final int UPPER_BOUND = 3;
     private final int LOWER_BOUND = -3;
     private static ScheduledExecutorService executorService;
 
+    /**
+     * Creates a new instance of Aktie with the specified name, wertpapierkennummer, and kurs.
+     * The Aktie object represents a stock with the given attributes.
+     *
+     * @param name                 the name of the stock
+     * @param wertpapierkennummer  the unique identifier for the stock
+     * @param kurs                 the initial value of the stock
+     */
     public Aktie(String name, int wertpapierkennummer, double kurs) {
         this.name = name;
         this.wertpapierkennummer = wertpapierkennummer;
@@ -27,10 +53,21 @@ public class Aktie {
         executorService.scheduleAtFixedRate(this::randKursGenerator, 0, 1, TimeUnit.SECONDS);
     }
 
+    /**
+     * Returns the unique identifier for this Aktie object.
+     *
+     * @return the wertpapierkennummer of the Aktie object
+     */
     public int getWertpapierkennummer() {
         return wertpapierkennummer;
     }
 
+    /**
+     * Generates a random price for the Aktie object.
+     * The generated price is in percentage, which is added to the current price.
+     * The generated price is a random value between the lower bound and upper boound.
+     *
+     */
     private void randKursGenerator() {
         Random r = new Random();
         double zufallProzent = LOWER_BOUND + (UPPER_BOUND - LOWER_BOUND) * r.nextDouble();
@@ -38,7 +75,36 @@ public class Aktie {
         this.kurs += this.kurs * zufallProzent / 100;
     }
 
+    /**
+     * Returns the current price of the Aktie object.
+     *
+     * @return The current price of the Aktie object.
+     */
     public double getKurs() {
         return kurs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWertpapierkennummer(int wertpapierkennummer) {
+        this.wertpapierkennummer = wertpapierkennummer;
+    }
+
+    public void setKurs(double kurs) {
+        this.kurs = kurs;
+    }
+
+    public static ScheduledExecutorService getExecutorService() {
+        return executorService;
+    }
+
+    public static void setExecutorService(ScheduledExecutorService executorService) {
+        Aktie.executorService = executorService;
     }
 }
